4a7ddb35ad99f4a2d1ec0edadc5d812663897041
Agnes <agnesfo@kth.se>
2022-02-01 19:21:27 +0100

    fix #6 documentation for LIC function

--- src/decide/Decider.java
+++ src/decide/Decider.java
@@ -39,6 +39,15 @@ public class Decider{
         return true;
     }
 
+
+    /** Returns a Boolean representing if there exists at least one set of Q_PTS consecutive data points
+     * that lie in more than QUADS quadrants. Where there is ambiguity as to which quadrant contains a given point,
+     * priority of decision will be by quadrant number, i.e., I, II, III, IV. For example, the data point (0,0)
+     * is in quadrant I, the point (-l,0) is in quadrant II, the point (0,-l) is in quadrant III, the point
+     * (0,1) is in quadrant I and the point (1,0) is in quadrant I.
+     * (2 ≤ Q_PTS ≤ NUMPOINTS), (1 ≤ QUADS ≤ 3)
+     *
+     * @return  Boolean representing if the condition is met or not.*/
     public boolean lic4(){
         // false in case of invalid parameters
         if(this.parameters.Q_PTS < 2 || this.parameters.Q_PTS > numpoints){
