a4baec809cadbf48e2b0ebb89a35b7841707357c
Thomas Couturou <thomas.couturou@eleves.ec-nantes.fr>
2022-02-01 16:14:40 +0100

    feat #4 LIC2 and tests

--- src/decide/Coordinate.java
+++ src/decide/Coordinate.java
@@ -9,5 +9,17 @@ public class Coordinate {
         this.y = y;
     }
 
+    public boolean isEgal(Coordinate P){
+        return this.x == P.x && this.y == P.y;
+    }
+
+    public double dist(Coordinate P){
+        return Math.sqrt(Math.pow(this.x - P.x, 2)+Math.pow(this.y - P.y, 2));
+    };
+
+    public double angle(Coordinate P1, Coordinate P2){
+        double scal  = (P1.x-this.x)*(P2.x-this.x)+(P1.y-this.y)*(P2.y-this.y);
+        return Math.acos(scal/(dist(P1)+dist(P2)));
+    }
     // Add functions for points here
 }
